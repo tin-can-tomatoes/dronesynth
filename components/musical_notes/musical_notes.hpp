97050536a5dd3136bd410a6d5cf8e93aeb5cbdcb
tomatoes <tomatoes@tin-can.ca>
2024-05-24 16:08:36 -0400

    increased note range (hopefully it works)

--- components/musical_notes/musical_notes.hpp
+++ components/musical_notes/musical_notes.hpp
@@ -6,8 +6,8 @@
 extern const uint16_t musical_notes[129];
 
 #define MUSICAL_NOTES_TOTAL 128
-#define MUSICAL_NOTE_MIN 36 // C2
-#define MUSICAL_NOTE_MAX 72 // C5
+#define MUSICAL_NOTE_MIN 12 // C0
+#define MUSICAL_NOTE_MAX 96 // C5
 
 
 
